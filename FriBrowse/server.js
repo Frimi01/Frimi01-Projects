5d344e53c571d7c13963214934aa12f361ea8799
Frimi01 <82447464+Frimi01@users.noreply.github.com>
2025-02-18 08:43:02 +0100

    Adding FriBrowse Project!

---
+++ FriBrowse/server.js
@@ -0,0 +1,81 @@
+//server.js
+require('dotenv').config();
+const express = require('express');
+const process = require('process')
+const fs = require('fs');
+const cors = require('cors');
+const { exec } = require("child_process"); //to open browser
+
+//config
+const app = express();
+const PORT = process.env.PORT;
+const SEARCH_ENGINE_ID = process.env.SEARCH_ENGINE_ID
+
+app.use(express.json()); // Allows JSON in requests
+app.use(cors({
+  origin: `http://localhost:${PORT}`
+})); // Allow frontend to call API
+
+app.use(express.static(process.cwd() + "/public")); //hosts files in public
+
+//Saves Bookmarks to local file
+app.post('/save-json', (req, res) => {
+  console.log("Received request to save bookmarks:", req.body);
+
+  const data = JSON.stringify(req.body, null, 2); // Format JSON nicely
+
+  fs.writeFile('./bookmarks.json', data, (err) => {
+    if (err) {
+      console.error('Error saving bookmarks:', err);
+      return res.status(500).json({ error: 'Failed to save bookmarks' });
+    }
+
+    console.log('Saved JSON successfully');
+    res.json({ message: 'Bookmarks saved successfully!' }); // Send response
+  });
+});
+
+//Sends Bookmarks to Frontend
+app.get('/get-bookmarks', (req, res) => {
+  console.log("Received request for bookmarks");
+
+  fs.readFile('./bookmarks.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading file:', err);
+      return res.status(500).json({ error: 'Failed to read bookmarks' });
+    }
+    res.json(JSON.parse(data)); // Send parsed JSON data
+    console.log('sent bookmarks')
+  });
+});
+
+// Sends API key to frontend
+app.get('/get-api-key', (req, res) => {
+  res.json({ apiKey: process.env.API_KEY, engineId: process.env.SEARCH_ENGINE_ID});
+  console.log('sent .env');
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`)
+
+    // Open browser depending on OS
+    const url = `http://localhost:${PORT}/`;
+    if (process.platform === "win32") {
+        exec(`start ${url}`); // Windows
+    } else if (process.platform === "darwin") {
+        exec(`open ${url}`); // macOS
+    } else if (process.platform === "linux") {
+        exec(`xdg-open ${url}`); // Linux
+    }
+});
+
+
+// Handle exit signals
+process.on("SIGINT", () => {
+  console.log("Shutting down server...");
+  server.close(() => {
+      console.log("Server closed.");
+      process.exit(0);
+  });
+});
